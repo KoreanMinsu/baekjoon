3f639a52f11881116127926810d35f8477ad86bd
김민수 <minsu.kim1988@gmail.com>
2021-06-15 23:58:58 +0900

    15/June/2021 not done yet.

---
+++ src/level1/input/output/Multiply2588.java
@@ -0,0 +1,17 @@
+package level1.input.output;
+
+import java.util.Scanner;
+
+public class Multiply2588 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		for(int i=0;i<3;i++) {
+			// 10/100/100의자리수 별 나머지 구하기
+		}
+		sc.close();
+		
+	}
+}
